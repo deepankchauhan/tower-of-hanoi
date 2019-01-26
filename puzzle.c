34ef86395d614021549b7356f04b07dad7e59fa6
Deepank Chauhan <deepankchauhan73@gmail.com>
2019-01-26 20:53:01 +0530

    review

--- puzzle.c
+++ puzzle.c
@@ -1,5 +1,8 @@
+// this program solves the problem of tower_of_hanoi
 #include <stdio.h>
 
+// function created for tower_of_hanoi.
+//  function tower_of_hanoi takes an integer n and three characters from_rod, to_rod and aux_rod as argument.
 int tower_of_hanoi(int n, char from_rod, char to_rod, char aux_rod)
 {
   if (n == 1)
@@ -7,13 +10,14 @@ int tower_of_hanoi(int n, char from_rod, char to_rod, char aux_rod)
     printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod)
     return 0;
   }
+  // this phenomenon shown below is called recursion, where the function is called within the function.
   tower_of_hanoi(n-1, from_rod, aux_rod, to_rod);
   printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod)
   tower_of_hanoi(n-1, aux_rod, to_rod, from_rod);  
 } 
 
 
-
+// main function
 int main(void)
 {
   int n;
