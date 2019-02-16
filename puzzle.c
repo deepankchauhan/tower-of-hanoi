4084379939c847eba8670aeb47590f2327098da8
Deepank Chauhan <deepankchauhan73@gmail.com>
2019-02-16 19:41:08 +0530

    Update puzzle.c

--- puzzle.c
+++ puzzle.c
@@ -4,17 +4,17 @@
 // function created for tower_of_hanoi.
 //  function tower_of_hanoi takes an integer n and three characters from_rod, to_rod and aux_rod as argument.
 int tower_of_hanoi(int n, char from_rod, char to_rod, char aux_rod)
-  {
-    if (n == 1)
-    {   
-      printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod)
-      return 0;
-    }
-    // this phenomenon shown below is called recursion, where the function is called within the function.
-    tower_of_hanoi(n-1, from_rod, aux_rod, to_rod);
-    printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod)
-    tower_of_hanoi(n-1, aux_rod, to_rod, from_rod);  
-  } 
+{
+  if (n == 1)
+  {   
+    printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod)
+    return 0;
+  }
+  // this phenomenon shown below is called recursion, where the function is called within the function.
+  tower_of_hanoi(n-1, from_rod, aux_rod, to_rod);
+  printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod)
+  tower_of_hanoi(n-1, aux_rod, to_rod, from_rod);  
+} 
 
 
 // main function
